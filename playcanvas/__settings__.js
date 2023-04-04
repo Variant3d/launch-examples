ee09c9059c4d3903526c7a5f65dff1288158e441
Ben Ferns <ben@benferns.io>
2023-04-04 12:14:41 +1000

    playcanvas example

---
+++ playcanvas/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1699112.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 126607276, 126607289, 126607294, 126607248, 126607247, 126607295, 126607296, 126607297, 126607299, 126607246, 126607300, 126607249, 126607251, 126607250 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/126607285/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/126607302/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/126607283/1/ammo.js', 'preload' : true},
+];
